b4223027d275fb35f9e1a94b05af1d482de8bd2f
Ben Augarten <b.augarten@gmail.com>
2013-12-18 11:32:57 -0700

    Added LoginViewController

---
+++ Notes/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Notes
+//
+//  Created by Ben Augarten on 12/17/13.
+//  Copyright (c) 2013 Ben Augarten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
